fc49be840eb70049fdb16a35848a92f91d1b23da
liltjhill <163649691+liltjhill@users.noreply.github.com>
2026-03-04 14:00:19 -0700

    Update assetlinks.json.js

--- functions/.well-known/assetlinks.json.js
+++ functions/.well-known/assetlinks.json.js
@@ -7,6 +7,7 @@ export async function onRequest() {
         package_name: "com.kidbank",
         sha256_cert_fingerprints: [
           "D0:BB:88:47:CB:0E:59:AC:31:6D:B4:51:6D:F0:F1:BD:D9:70:29:7D:40:AA:9E:C2:E2:46:A4:B9:1C:74:78:C8",
+          "FA:9D:13:29:69:22:E1:40:83:5C:F9:74:AD:49:18:33:D3:39:19:85:5B:21:A8:2F:9C:B2:07:7C:60:AE:C5:2E",
         ],
       },
     },
@@ -16,6 +17,7 @@ export async function onRequest() {
     status: 200,
     headers: {
       "content-type": "application/json; charset=utf-8",
+      // optional but nice:
       "cache-control": "public, max-age=300",
     },
   });
